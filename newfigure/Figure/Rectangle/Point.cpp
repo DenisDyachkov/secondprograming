bbb7ebc27bc660f9ec8a02bc8def69e1363862ca
Denis <kimak911@mail.ru>
2023-04-21 13:55:41 +0300

    Переделал #1

---
+++ newfigure/Figure/Rectangle/Point.cpp
@@ -0,0 +1,22 @@
+﻿#include "Point.h"
+#include <string>
+
+miit::figure::Point::Point(const double user_x, const double user_y)
+	: x(user_x) , y(user_y)
+{
+}
+
+const double miit::figure::Point::get_x() const
+{
+	return this->x;
+}
+
+const double miit::figure::Point::get_y() const
+{
+	return this->y;
+}
+
+std::ostream& miit::figure::operator<<(std::ostream& os, Point& point)
+{
+	return os << "x: " + std::to_string(point.x) + " y: " + std::to_string(point.y);
+}
